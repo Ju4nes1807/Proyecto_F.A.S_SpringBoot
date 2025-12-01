b357ea60af856cfc58e2c45478c1496c2287420c
FoldingZoo <stonecave77715@gmail.com>
2025-12-01 13:37:03 -0500

    DTO enrenamientos y model

---
+++ src/main/java/com/fas/project/dto/user/EntrenamientoCreateDTO.java
@@ -0,0 +1,27 @@
+package com.fas.project.dto.user;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EntrenamientoCreateDTO {
+
+    private String titulo;
+    private LocalDate fecha;
+    private LocalTime hora_inicio;
+    private LocalTime hora_fin;
+    private String lugar;
+    private String descripcion;
+    private Integer id_categoria;
+}
+
